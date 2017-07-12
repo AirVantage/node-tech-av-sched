7048216ccc0dedd602284ba3cce59a032a7902be
Simon Gaestel <simon.gaestel+git@gmail.com>
2017-07-12 18:00:37 +0200

    PLTFRS-8006 - Format with prettier (#2)
    * PLTFRS-8006 - Use prettier as formatter and eslint as linter
    
    * PLTFRS-8006 - Format with prettier using 2 spaces tabs
    
    * PLTFRS-8006 - Format with prettier using 2 spaces tabs
    
    * PLTFRS-8006 - One liners + template strings for readability

--- techAvSched.js
+++ techAvSched.js
@@ -4,55 +4,44 @@
  * @param options.secret
  */
 module.exports = function(options, http) {
-    return {
-
-        /**
+  return {
+    /**
          * Schedule a job
          *
          * @param  {Object}     requestBody     The job definition
          */
-        scheduleJob: function(ruuid, requestBody) {
-            return http.post(ruuid, {
-                url: options.url + "/sched/api/job-def",
-                body: requestBody,
-                headers: {
-                    "X-Sched-secret": options.secret
-                }
-            });
-        },
+    scheduleJob: function(ruuid, requestBody) {
+      return http.post(ruuid, {
+        url: `${options.url}/sched/api/job-def`,
+        body: requestBody,
+        headers: { 'X-Sched-secret': options.secret }
+      });
+    },
 
-        /**
+    /**
          * Acknowledge a job
          *
          * @param  {int}     jobId      The uid of the job to acknowledge
          */
-        ackJob: function(ruuid, jobId) {
-            return http.post(ruuid, {
-                url: options.url + "/sched/api/job-action/ack",
-                body: {
-                    id: jobId
-                },
-                headers: {
-                    "X-Sched-secret": options.secret
-                }
-            });
-        },
+    ackJob: function(ruuid, jobId) {
+      return http.post(ruuid, {
+        url: `${options.url}/sched/api/job-action/ack`,
+        body: { id: jobId },
+        headers: { 'X-Sched-secret': options.secret }
+      });
+    },
 
-        /**
+    /**
          * Delete a job
          *
          * @param  {int}     jobId      The uid of the job to acknowledge
          */
-        deleteJob: function(ruuid, jobId) {
-            return http.delete(ruuid, {
-                url: options.url + "/sched/api/job-def",
-                body: {
-                    id: jobId
-                },
-                headers: {
-                    "X-Sched-secret": options.secret
-                }
-            });
-        }
-    };
+    deleteJob: function(ruuid, jobId) {
+      return http.delete(ruuid, {
+        url: `${options.url}/sched/api/job-def`,
+        body: { id: jobId },
+        headers: { 'X-Sched-secret': options.secret }
+      });
+    }
+  };
 };
