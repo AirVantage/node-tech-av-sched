cc8ef3534f00017e8d52ce522a0e08a5e0ce583d
sgaestel <sgaestel@sierrawireless.com>
2018-09-14 11:32:52 +0200

    Remove ruuid + use Got

--- techAvSched.js
+++ techAvSched.js
@@ -1,47 +1,46 @@
-/**
- *
- * @param options.url
- * @param options.secret
- */
-module.exports = function(options, http) {
-  return {
-    /**
-         * Schedule a job
-         *
-         * @param  {Object}     requestBody     The job definition
-         */
-    scheduleJob: function(ruuid, requestBody) {
-      return http.post(ruuid, {
-        url: `${options.url}/sched/api/job-def`,
-        body: requestBody,
-        headers: { 'X-Sched-secret': options.secret }
-      });
-    },
+const got = require('got');
 
-    /**
-         * Acknowledge a job
-         *
-         * @param  {int}     jobId      The uid of the job to acknowledge
-         */
-    ackJob: function(ruuid, jobId) {
-      return http.post(ruuid, {
-        url: `${options.url}/sched/api/job-action/ack`,
-        body: { id: jobId },
-        headers: { 'X-Sched-secret': options.secret }
-      });
-    },
+class AvSched {
+  /**
+   * @param setup.url
+   * @param setup.secret
+   */
+  constructor(setup) {
+    this.got = got.extend({
+      baseUrl: setup.url,
+      json: true,
+      headers: {
+        'X-Sched-secret': setup.secret
+      }
+    });
+  }
 
-    /**
-         * Delete a job
-         *
-         * @param  {int}     jobId      The uid of the job to acknowledge
-         */
-    deleteJob: function(ruuid, jobId) {
-      return http.delete(ruuid, {
-        url: `${options.url}/sched/api/job-def`,
-        body: { id: jobId },
-        headers: { 'X-Sched-secret': options.secret }
-      });
-    }
-  };
-};
+  /**
+   * Schedule a job
+   *
+   * @param  {Object}     requestBody     The job definition
+   */
+  scheduleJob(body) {
+    this.got.post('/sched/api/job-def', { body });
+  }
+
+  /**
+   * Acknowledge a job
+   *
+   * @param  {int}     jobId      The uid of the job to acknowledge
+   */
+  ackJob(jobId) {
+    this.got.post('/sched/api/job-action/ack', { body: { id: jobId } });
+  }
+
+  /**
+   * Delete a job
+   *
+   * @param  {int}     jobId      The uid of the job to acknowledge
+   */
+  deleteJob(jobId) {
+    this.got.delete('/sched/api/job-def', { body: { id: jobId } });
+  }
+}
+
+module.exports = AvSched;
